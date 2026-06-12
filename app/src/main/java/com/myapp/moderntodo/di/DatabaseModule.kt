f25c5c0c85482076285caeba4abb3c8bc50e4199
Vladislav Lifirenko <vladislav.lifirenko@gmail.com>
2026-06-12 10:11:21 +0300

    DI

--- app/src/main/java/com/myapp/moderntodo/di/DatabaseModule.kt
+++ app/src/main/java/com/myapp/moderntodo/di/DatabaseModule.kt
@@ -1,4 +1,44 @@
 package com.myapp.moderntodo.di
 
-class DatabaseModule {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import com.myapp.moderntodo.data.local.dao.TaskDao
+import com.myapp.moderntodo.data.local.db.AppDatabase
+import com.myapp.moderntodo.data.repository.TaskRepository
+import com.myapp.moderntodo.data.repository.TaskRepositoryImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideAppDatabase(
+        @ApplicationContext context: Context
+    ): AppDatabase {
+        return Room.databaseBuilder(
+            context,
+            AppDatabase::class.java,
+            "todo_databse"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideTaskDao(database: AppDatabase): TaskDao {
+        return database.taskDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideTaskRepository(taskDao: TaskDao): TaskRepository {
+        return TaskRepositoryImpl(taskDao)
+    }
 }
